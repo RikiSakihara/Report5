2071ae6b2cff40bbf2d71ecf1f5e5bbb2ca2f3c0
RIki Sakihara <e245722@ie.u-ryukyu.ac.jp>
2024-12-12 14:34:20 +0900

    3nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        try{
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("//条件2\n"+"NullPointerExceptionが発生した");
-            System.out.println("//条件3\n"+e.getMessage());
-        }
+        String str = "壱百満";
+        Integer.parseInt(str);
     }
 }
+
